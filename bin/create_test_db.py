4ff4c91fb427d528a87c3dcb7bd4233ffc577b22
Kevin Fenger <kevinfenger1@Kevins-MacBook-Air.local>
2015-04-28 15:17:52 -0600

    moving some files around

--- bin/create_test_db.py
+++ bin/create_test_db.py
@@ -4,23 +4,8 @@ from cassandra.cluster import Cluster
 
 sys.path.append(os.getcwd())
 from cqlengine.management import sync_table 
-from app.api.models.connection import connect 
+from app.api.connection import connect 
 connect()
 
-from app.api.models.videos import Video 
+from app.api.mod_video.models import Video 
 sync_table(Video)
-#from app.api.models.connection import connect
-#connect()
-#try: 
-#    cluster = Cluster()
-#    session = cluster.connect()
-#except Exception as e: 
-#    print 'can not connect to a cluster'
-
-#try: 
-#    session.execute("CREATE KEYSPACE IF NOT EXISTS videos WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }")
-#    session.execute("USE videos")
-#    session.execute("CREATE TABLE IF NOT EXISTS vids (video_id text, images text, PRIMARY KEY (video_id)) WITH caching = '{\"keys\":\"ALL\", \"rows_per_partition\":\"10\"}'")
-#except Exception as e: 
-#    print e
-#    print 'something went wrong in setting up the DB' 
