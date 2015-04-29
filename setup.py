81dfe43eaceaf54c286c02d73b5818e64aa3fdfc
Kevin Fenger <kevindfenger@gmail.com>
2015-04-28 22:30:42 -0600

    adding setup script and manifest

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='RABiT',
+    name='rabit ',
     version='0.1',
     long_description=__doc__,
-    packages=['app'],
+    packages=['api'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask', 'cassandra']
+    install_requires=['Flask', 'cassandra-driver']
 )
